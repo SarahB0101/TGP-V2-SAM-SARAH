725b45e23d06d842aed7fa1dd897b1408b50112e
SarahB0101 <sbouchir@gmail.com>
2019-08-06 16:26:09 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+ 	
+  # get 'gossips/new', to: 'gossips#new' #on affiche le formulaire
+  # post 'gossips', to: 'gossips#create' #puis on l'envoie
+  resources :gossips
+  resources :users, only: [:index]
+  #get 'users/:id', to:'user#show', as: 'users'
+  #get 'gossips/:id', to:'gossip#show', as: 'gossips'
+  get 'welcome/:user_entry', to: 'welcome#show'
+  get '/index', to: 'index#show'
+  get 'contact', to: 'contact#show'
+  get 'team', to: 'team#show' 
+end
+  
+
